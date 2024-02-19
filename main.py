c9d83dbb0b06d96e6ac5e62c80d315669dc7fd68
Panisset42 <lucas.lima319@etec.sp.gov.br>
2024-02-19 09:38:36 -0300

    Cty_register and model register screens made. Starting to work on LP generation screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # main.py
-from layout import MinhaInterface
+from layout import MinhaInterface, EventPublisher
+
 
 def handle_login_data(username, password, login_frame):
     if username == "asd" and password == "asd":
@@ -7,6 +8,26 @@ def handle_login_data(username, password, login_frame):
     else:
         return False
 
+
+def handle_city_register(city_name, city_date):
+    print(city_name)
+    print(city_date)
+
+def handle_model_register(model_name, model_link, model_archive):
+    print(model_name)
+    print(model_link)
+    print(model_archive)
+
+
+def handle_login_attempt(username, password):
+   if username == "asd" and password == "asd":
+       app.main_screen()
+
+
+EventPublisher.subscribe("login_attempt", handle_login_attempt)
+EventPublisher.subscribe("city_register_clicked", handle_city_register)
+EventPublisher.subscribe("model_register_clicked", handle_model_register)
+
 app = MinhaInterface()
-app.login_screen(handle_login_data)
+app.login_screen()
 app.janela.mainloop()
